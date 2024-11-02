9f9843731fce13d5ea8f8dbb7bf6b2ec3d582001
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-11-02 20:05:38 +0900

    ✨ [Feat] loading.js spinner 추가

--- app/loading.js
+++ app/loading.js
@@ -1,3 +1,9 @@
-export default function loading() {
-  return <h1>Now Loading...</h1>;
+import classes from "./loading.module.css";
+
+export default function Loading() {
+  return (
+    <div className={classes.loadingContainer}>
+      <div className={classes.spinner}></div>
+    </div>
+  );
 }
