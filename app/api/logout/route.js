9173a395c23333eedb6711912996a253bcb265d3
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-11-04 18:45:43 +0900

    🐛 [BugFix] 에러처리 추가

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -1,15 +1,22 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const response = NextResponse.json({ message: "Logged out successfully" });
+  try {
+    const response = NextResponse.json({ message: "Logged out successfully" });
 
-  response.cookies.set("token", "", {
-    path: "/",
-    maxAge: 0,
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-  });
+    response.cookies.set("token", "", {
+      path: "/",
+      maxAge: 0,
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
 
-  return response;
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Failed to fetch", error: error.message },
+      { status: 500 }
+    );
+  }
 }
