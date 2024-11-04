f884473ea8fbdee7575c0e3df5370dbe5f86bb09
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-11-04 18:51:34 +0900

    🐛 [BugFix] 쿠키에 에러처리 추가

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -4,13 +4,20 @@ export async function GET() {
   try {
     const response = NextResponse.json({ message: "Logged out successfully" });
 
-    response.cookies.set("token", "", {
-      path: "/",
-      maxAge: 0,
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    });
+    try {
+      response.cookies.set("token", "", {
+        path: "/",
+        maxAge: 0,
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
+    } catch (cookieError) {
+      return NextResponse.json(
+        { message: "Failed to delete cookie", error: cookieError.message },
+        { status: 500 }
+      );
+    }
 
     return response;
   } catch (error) {
