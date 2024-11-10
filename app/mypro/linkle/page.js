d72ae9aa1bc8c56b43c15085821da23adbc76398
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-11-10 13:56:30 +0900

    ㅇㅇ

--- app/mypro/linkle/page.js
+++ app/mypro/linkle/page.js
@@ -3,9 +3,9 @@ import classes from "./page.module.css";
 export default async function SomeThing() {
   return (
     <div className={classes.title}>
-      <h1>데이터임</h1>
-      <h2></h2>
-      <h3></h3>
+      <h1>ㅇㅇ</h1>
+      <h2>ㅇㅇ</h2>
+      <h3>ㅇㅇ</h3>
     </div>
   );
 }
