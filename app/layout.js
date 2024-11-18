3a6a6ad182bcae0d3d8a570f4fcb0e0343baa1d3
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-11-18 14:52:31 +0900

    ✨ [Feat] google analytics 시험

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Mainheader from "../components/Main-header";
+import Head from "next/head";
 
 export const metadata = {
   title: "Defor's Portfolio",
@@ -9,15 +10,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-9B6HE3CWDK"
-      ></script>
-      <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-9B6HE3CWDK');
-      </script>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9B6HE3CWDK"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9B6HE3CWDK');
+            `,
+          }}
+        />
+      </Head>
       <body className="layout">
         <Mainheader />
         {children}
